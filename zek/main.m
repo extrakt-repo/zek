89985e89ce673ba57d3ad325a0b37c4beac7b374
exTrakt <exTrakt@Jeffreys-iMac.local>
2014-04-06 18:01:35 -0500

    Initial Commit

---
+++ zek/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  zek
+//
+//  Created by exTrakt on 4/6/14.
+//  Copyright (c) 2014 exTrakt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "zekAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([zekAppDelegate class]));
+    }
+}
---
+++ zek/zekAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  zekAppDelegate.h
+//  zek
+//
+//  Created by exTrakt on 4/6/14.
+//  Copyright (c) 2014 exTrakt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface zekAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
